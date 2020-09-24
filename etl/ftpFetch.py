3c6d61b9abb8240448a6118eb9f56af84c219996
Eric Lohmar <Eric.Lohmar@aa.com>
2020-09-24 09:58:07 -0500

    Added ftp to func for skd prep

--- etl/ftpFetch.py
+++ etl/ftpFetch.py
@@ -31,59 +31,62 @@ def cleanAndAppend(line):
     if line.endswith('.txt'):
         files.append(line[line.rindex(' ')+1:])
 
+def ftpMain():
+    with FTP() as ftp:
+        ftp.connect(params.ftpPxy, ftpPrt)
+        ftp.login(f'{params.ftpUsr}@ftp.teradata.com', params.ftpPwd)
+        ftp.cwd('xfer')
+        ftp.dir(cleanAndAppend)
 
-with FTP() as ftp:
-    ftp.connect(params.ftpPxy, ftpPrt)
-    ftp.login(f'{params.ftpUsr}@ftp.teradata.com', params.ftpPwd)
-    ftp.cwd('xfer')
-    ftp.dir(cleanAndAppend)
+        for fname in files:
+            with open(rf'{outputDir}\{fname}', 'w') as f:
+                def writeLine(line):
+                    try:
+                        f.write(line)
+                    except UnicodeEncodeError:
+                        line = ''.join(c for c in line if c.isprintable())
+                    f.write('\n')
+                ftp.retrlines(f'RETR {fname}', writeLine)
+            printUpd(f'{fname} Downloaded')
 
-    for fname in files:
-        with open(rf'{outputDir}\{fname}', 'w') as f:
-            def writeLine(line):
-                try:
-                    f.write(line)
-                except UnicodeEncodeError:
-                    line = ''.join(c for c in line if c.isprintable())
-                f.write('\n')
-            ftp.retrlines(f'RETR {fname}', writeLine)
-        printUpd(f'{fname} Downloaded')
+    with td.connect(
+        host=params.MyHost,
+        user=params.MyUser,
+        password=params.Password,
+        logmech=params.LogMech
+    ) as con:
+        for fname in files:
+            tbl = f'{params.SchemaName}.{fname[:fname.rindex(".")]}'
+            with open(rf'{outputDir}\{fname}', newline='') as f:
+                data = [
+                    [None if not item else item for item in row]
+                    for row in csv.reader(f, delimiter='|')
+                    if row
+                ]
+            header = data.pop(0)
+            colList = ','.join(f'"{col}"' for col in header)
+            paramList = ','.join(['?'] * len(header))
 
-with td.connect(
-    host=params.MyHost,
-    user=params.MyUser,
-    password=params.Password,
-    logmech=params.LogMech
-) as con:
-    for fname in files:
-        tbl = f'{params.SchemaName}.{fname[:fname.rindex(".")]}'
-        with open(rf'{outputDir}\{fname}', newline='') as f:
-            data = [
-                [None if not item else item for item in row]
-                for row in csv.reader(f, delimiter='|')
-                if row
-            ]
-        header = data.pop(0)
-        colList = ','.join(f'"{col}"' for col in header)
-        paramList = ','.join(['?'] * len(header))
+            flSetupStmnt = "{fn teradata_nativesql}{fn teradata_autocommit_off}"
+            delStmnt = f'delete from {tbl}'
+            insStmnt = (
+                f'{"{fn teradata_try_fastload}" if len(data) > 1e6 else ""}'
+                f'insert into {tbl} ({colList}) values ({paramList})'
+            )
+            try:
+                with con.cursor() as cur:
+                    cur.execute(flSetupStmnt)
+                    cur.execute(delStmnt)
+                    cur.execute(insStmnt, data)
+                    con.commit()
+                printUpd(f'{fname} Inserted')
+            except td.OperationalError as e:
+                if 'does not exist' in str(e):
+                    printUpd(f'--> {fname} Skipped')
+                else:
+                    raise e
 
-        flSetupStmnt = "{fn teradata_nativesql}{fn teradata_autocommit_off}"
-        delStmnt = f'delete from {tbl}'
-        insStmnt = (
-            f'{"{fn teradata_try_fastload}" if len(data) > 1e6 else ""}'
-            f'insert into {tbl} ({colList}) values ({paramList})'
-        )
-        try:
-            with con.cursor() as cur:
-                cur.execute(flSetupStmnt)
-                cur.execute(delStmnt)
-                cur.execute(insStmnt, data)
-                con.commit()
-            printUpd(f'{fname} Inserted')
-        except td.OperationalError as e:
-            if 'does not exist' in str(e):
-                printUpd(f'--> {fname} Skipped')
-            else:
-                raise e
+    stgToCore()
 
-stgToCore()
+if __name__ == "__main__":
+    ftpMain()
