da1e067ece9ae260d6c497b5835da6a6b93791a9
Eric Lohmar <Eric.Lohmar@aa.com>
2020-09-24 11:04:15 -0500

    Correcting func name

--- etl/common.py
+++ etl/common.py
@@ -3,5 +3,5 @@ import datetime as dt
 def print_complete(s):
     print(f'{s}: {dt.datetime.now().isoformat(sep=" ", timespec="seconds")}')
 
-def print_header(s):
+def header(s):
     return s.center(80, '-')
